80806f83f4b25857be8b3c6cc27a10432116eb1c
LeRe <levchenko.rp@gmail.com>
2017-03-10 17:06:08 +0300

    0

--- app/src/main/java/ru/ijava/tyrepressureconversion/MainActivity.java
+++ app/src/main/java/ru/ijava/tyrepressureconversion/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +27,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        EditText editBar = (EditText) findViewById(R.id.editBar);
+        final EditText editPsi = (EditText) findViewById(R.id.editPsi);
+
+        editBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                editPsi.setText(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
