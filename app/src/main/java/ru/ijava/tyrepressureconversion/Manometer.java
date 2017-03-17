eb74f88d6022f62f643dd676d40f257a0ffdaba2
LeRe <levchenko.rp@gmail.com>
2017-03-17 17:45:51 +0300

    0

--- app/src/main/java/ru/ijava/tyrepressureconversion/Manometer.java
+++ app/src/main/java/ru/ijava/tyrepressureconversion/Manometer.java
@@ -4,10 +4,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.RectF;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
 import android.util.DisplayMetrics;
 import android.view.View;
 
@@ -17,9 +17,17 @@ import android.view.View;
 
 public class Manometer extends View {
     private int sideSize;
+    private final int startAngle = 135;
+    private final int sweepAngle = 270;
 
-    private Paint p = new Paint();
-    private RectF rectF = new RectF(0, 0, sideSize, sideSize);
+    private final int padding = 100;
+
+    private Paint p;
+    private RectF rectF;
+    private Path pathHatch;
+    private Path pathText;
+    private Matrix matrixHatch;
+    private Matrix matrixText;
 
     public int getSideSize() {
         return sideSize;
@@ -43,27 +51,63 @@ public class Manometer extends View {
         }
 
         p = new Paint();
-        rectF = new RectF(0, 0, sideSize, sideSize);
+        p.setAntiAlias(true);
+        p.setStyle(Paint.Style.STROKE);
+        p.setStrokeWidth(5);
+        p.setTextSize(60);
 
+        rectF = new RectF(padding, padding, sideSize - padding, sideSize - padding);
+        pathHatch = new Path();
+        pathText = new Path();
+        matrixHatch = new Matrix();
+        matrixText = new Matrix();
 
         setFocusable(true);
     }
 
     protected void onDraw(Canvas canvas) {
+        // paint background
+        //canvas.drawColor(Color.CYAN);
 
-        canvas.drawColor(Color.CYAN);
+        //drawArc
+        p.setColor(Color.WHITE);
+        canvas.drawArc(rectF, startAngle, sweepAngle, false, p);
 
-        // smooths
-        p.setAntiAlias(true);
+        //draw hatch
+        int hatchSize = 30;
+        //canvas.drawLine(sideSize/2, 0 + padding, sideSize/2, sideSize/2 + hatchSize, p);
+        pathHatch.reset();
+        pathHatch.moveTo(sideSize/2, padding - hatchSize);
+        pathHatch.lineTo(sideSize/2, padding);
+
+        pathText.reset();
+        pathText.moveTo(sideSize/2, padding - hatchSize - hatchSize/4);
+        pathText.lineTo(sideSize/2+1, padding - hatchSize - hatchSize/4);
+
+        int angle = -135;
+        for(int i=0; i<=6; i++){
+            matrixHatch.reset();
+            matrixHatch.setRotate(angle, sideSize/2, sideSize/2);
+            pathHatch.transform(matrixHatch);
+
+            matrixText.reset();
+            matrixText.setRotate(angle, sideSize/2, sideSize/2);
+            matrixText.setSinCos((float) 0.1, (float) 0.9);
+
+            pathText.transform(matrixText);
+            canvas.drawPath(pathHatch, p);
+            canvas.drawTextOnPath(String.valueOf(i), pathText, 0, 0, p);
+            angle = 45;
+        }
+
+
+        // draw arrow
+        int centralRadius = 10;
+        int shiftArrow = 40;
         p.setColor(Color.RED);
-        p.setStyle(Paint.Style.STROKE);
-        p.setStrokeWidth(5);
-        // opacity
-        //p.setAlpha(0x80); //
+        canvas.drawCircle(sideSize/2, sideSize/2, centralRadius, p);
+        canvas.drawLine(sideSize/2, sideSize/2 + shiftArrow, sideSize/2, padding + shiftArrow, p);
 
-        canvas.drawOval(rectF, p);
-        p.setColor(Color.BLACK);
-        canvas.drawArc (rectF, 90, 45, true, p);
     }
 
 }
